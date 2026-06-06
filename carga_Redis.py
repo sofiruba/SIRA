66d1ab31bc51ece176742619a46e23bd673c516c
Facundo Castillo Ivanauskas <facundocastilloiva@gmail.com>
2026-06-06 16:32:16 -0300

    Cambio de carga_redis

--- carga_Redis.py
+++ carga_Redis.py
@@ -1,73 +1,112 @@
 import redis
 import json
 
-# Conexión a Redis
+# ==========================================
+# CONEXIÓN A REDIS, docker run --name redis -p 6379:6379 -d redis
+# ==========================================
+
 r = redis.Redis(
     host="localhost",
     port=6379,
     decode_responses=True
 )
 
+# ==========================================
+# CARGA DE DATOS
+# ==========================================
 
-# Limpiar Redis antes de cargar
-r.flushdb()
+def cargar_datos(ruta_json="redis_data.json"):
 
-# Leer archivo JSON
-with open("redis_data.json", "r", encoding="utf-8") as f:
-    datos = json.load(f)
+    with open(ruta_json, "r", encoding="utf-8") as archivo:
+        datos = json.load(archivo)
 
-# Cargar puntos verdes
-for punto in datos["puntos_verdes"]:
+    # Limpiar la base de datos antes de cargar nuevos datos, esto para evitar duoplicados 
+    r.flushdb()
 
-    key = f"puntoverde:{punto['_id']}:estado"
+    # Carga de puntos verdes en hashes y sets para estados
+    for punto in datos["puntos_verdes"]:
 
-    r.hset(
-        key,
-        mapping={
-            "estado": punto["estado"],
-            "capacidad_actual": punto["capacidad_actual"],
-            "ultima_actualizacion": punto["ultima_actualizacion"]
-        }
+        punto_id = punto["id"]
+
+        r.hset(
+            f"puntoverde:{punto_id}:estado",
+            mapping={
+                "estado": punto["estado"],
+                "capacidad_actual": punto["capacidad_actual"],
+                "ultima_actualizacion": punto["ultima_actualizacion"]
+            }
+        )
+
+        if punto["estado"] == "disponible":
+            r.sadd("puntosverdes:disponibles", punto_id)
+
+        elif punto["estado"] == "saturado":
+            r.sadd("puntosverdes:saturados", punto_id)
+
+    # carga de ranking en un sorted set 
+    for ranking in datos["ranking_uso"]:
+
+        r.zadd(
+            "ranking:puntosverdes:uso",
+            {
+                str(ranking["id_punto_verde"]):
+                ranking["puntaje"]
+            }
+        )
+
+    # carga de alertas en una lista (list)
+    for alerta in datos["alertas"]:
+
+        r.rpush(
+            "alertas:recientes",
+            json.dumps(alerta, ensure_ascii=False)
+        )
+
+# ==========================================
+# CONSULTAS
+# ==========================================
+
+def obtener_estado_punto_verde(id_punto):
+    return r.hgetall(
+        f"puntoverde:{id_punto}:estado"
     )
 
+def obtener_puntos_disponibles():
+    return list(
+        r.smembers("puntosverdes:disponibles")
+    )
 
-#SETs para puntos verdes disponibles y saturados
-for punto in datos["puntos_verdes"]:
+def obtener_puntos_saturados():
+    return list(
+        r.smembers("puntosverdes:saturados")
+    )
 
-    punto_id = punto["_id"]
-    estado = punto["estado"]
-
-    if estado == "disponible":
-
-        r.sadd(
-            "puntosverdes:disponibles",
-            punto_id
-        )
-
-    elif estado == "saturado":
-
-        r.sadd(
-            "puntosverdes:saturados",
-            punto_id
-        )
-
-#Carga de ranking de uso
-for ranking in datos["ranking_uso"]:
-
-    r.zadd(
+def obtener_ranking():
+    return r.zrevrange(
         "ranking:puntosverdes:uso",
-        {
-            str(ranking["id_punto_verde"]):
-            ranking["puntaje"]
-        }
+        0,
+        -1,
+        withscores=True
     )
 
-#Carga de alertas recientes
-for alerta in datos["alertas"]:
-
-    r.rpush(
+def obtener_alertas():
+    alertas = r.lrange(
         "alertas:recientes",
-        json.dumps(alerta, ensure_ascii=False)
+        0,
+        -1
     )
 
-print("Datos cargados correctamente")
+    return [
+        json.loads(alerta)
+        for alerta in alertas
+    ]
+
+# ==========================================
+# EJECUCION A LO CRIOLLO, se ejecuta de forma manual desde la terminal para carga de datos
+# ==========================================
+
+if __name__ == "__main__":
+
+    cargar_datos()
+
+    print("Datos cargados correctamente en Redis.")
